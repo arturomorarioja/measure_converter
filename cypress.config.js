72125fe8787e3232066f8838fa725d8d47655b66
Arturo Mora-Rioja <arturomora@gmail.com>
2023-04-18 09:01:15 +0200

    Fixed syntax error in Cypress config

--- cypress.config.js
+++ cypress.config.js
@@ -3,12 +3,12 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:80',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
   integration: {
     
-  },
-  baseUrl: 'http://localhost:80'
+  }
 });
